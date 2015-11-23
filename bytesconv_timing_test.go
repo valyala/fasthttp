5e36ee0947dd5f1e0e7b944d1b701781faf3ba85
Aliaksandr Valialkin <valyala@gmail.com>
2015-11-23 16:58:29 +0200

    Added benchmarks for lowercaseBytes

---
+++ bytesconv_timing_test.go
@@ -0,0 +1,38 @@
+package fasthttp
+
+import (
+	"testing"
+)
+
+func BenchmarkLowercaseBytesNoop(b *testing.B) {
+	src := []byte("foobarbaz_lowercased_all")
+	b.RunParallel(func(pb *testing.PB) {
+		s := make([]byte, len(src))
+		for pb.Next() {
+			copy(s, src)
+			lowercaseBytes(s)
+		}
+	})
+}
+
+func BenchmarkLowercaseBytesAll(b *testing.B) {
+	src := []byte("FOOBARBAZ_UPPERCASED_ALL")
+	b.RunParallel(func(pb *testing.PB) {
+		s := make([]byte, len(src))
+		for pb.Next() {
+			copy(s, src)
+			lowercaseBytes(s)
+		}
+	})
+}
+
+func BenchmarkLowercaseBytesMixed(b *testing.B) {
+	src := []byte("Foobarbaz_Uppercased_Mix")
+	b.RunParallel(func(pb *testing.PB) {
+		s := make([]byte, len(src))
+		for pb.Next() {
+			copy(s, src)
+			lowercaseBytes(s)
+		}
+	})
+}
