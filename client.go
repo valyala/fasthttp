0fa8e8efc76f5787295a6ef0381b9ed606001dee
Aliaksandr Valialkin <valyala@gmail.com>
2015-11-25 14:30:51 +0200

    Client: dial both tcp4 and tcp6 addresses

--- client.go
+++ client.go
@@ -949,7 +949,7 @@ func (c *HostClient) defaultDialFunc(addr string) (net.Conn, error) {
 	if err != nil {
 		return nil, err
 	}
-	return net.DialTCP("tcp4", nil, tcpAddr)
+	return net.DialTCP("tcp", nil, tcpAddr)
 }
 
 func (c *HostClient) getTCPAddr(addr string) (*net.TCPAddr, error) {
