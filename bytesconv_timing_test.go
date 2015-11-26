b31fd30964a021e1c719f8b2155bc891bcefb959
Aliaksandr Valialkin <valyala@gmail.com>
2015-11-26 17:57:44 +0200

    Added benchmark for comparing unequal strings with EqualBytesStr

--- bytesconv_timing_test.go
+++ bytesconv_timing_test.go
@@ -37,13 +37,26 @@ func BenchmarkLowercaseBytesMixed(b *testing.B) {
 	})
 }
 
-func BenchmarkEqualBytesStr(b *testing.B) {
+func BenchmarkEqualBytesStrEq(b *testing.B) {
 	s := "foobarbaraz"
 	bs := []byte(s)
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
 			if !EqualBytesStr(bs, s) {
 				b.Fatalf("unexpected result: %q != %q", bs, s)
+			}
+		}
+	})
+}
+
+func BenchmarkEqualBytesStrNe(b *testing.B) {
+	s := "foobarbaraz"
+	bs := []byte(s)
+	bs[len(s)-1] = 'a'
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			if EqualBytesStr(bs, s) {
+				b.Fatalf("unexpected result: %q = %q", bs, s)
 			}
 		}
 	})
