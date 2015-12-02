0b6cc4cbad3c6b833efce87c622afc1e88700c6e
Aliaksandr Valialkin <valyala@gmail.com>
2015-12-02 20:32:49 +0200

    Issue #13: add a (k,v) storage inside RequestCtx for user values, which may pass between middlewares

--- server.go
+++ server.go
@@ -237,6 +237,8 @@ type RequestCtx struct {
 	// Copying Response by value is forbidden. Use pointer to Response instead.
 	Response Response
 
+	userValues map[string]interface{}
+
 	id uint64
 
 	lastReadDuration time.Duration
@@ -291,6 +293,36 @@ type HijackHandler func(c net.Conn)
 //
 func (ctx *RequestCtx) Hijack(handler HijackHandler) {
 	ctx.hijackHandler = handler
+}
+
+// SetUserValue stores the given value (arbitrary object)
+// under the given key in ctx.
+//
+// The value stored in ctx may be obtained by UserValue().
+//
+// This functionality may be useful for passing arbitrary values between
+// functions involved in request processing.
+//
+// All the values stored in ctx are deleted after returning from RequestHandler.
+func (ctx *RequestCtx) SetUserValue(key string, value interface{}) {
+	if ctx.userValues == nil {
+		ctx.userValues = make(map[string]interface{}, 1)
+	}
+	ctx.userValues[key] = value
+}
+
+// UserValue returns the value stored via SetUserValue under the given key.
+func (ctx *RequestCtx) UserValue(key string) interface{} {
+	if ctx.userValues == nil {
+		return nil
+	}
+	return ctx.userValues[key]
+}
+
+func (ctx *RequestCtx) resetUserValues() {
+	for k := range ctx.userValues {
+		delete(ctx.userValues, k)
+	}
 }
 
 // IsTLS returns true if the underlying connection is tls.Conn.
@@ -918,6 +950,8 @@ func (s *Server) serveConn(c net.Conn) error {
 
 		hijackHandler = ctx.hijackHandler
 		ctx.hijackHandler = nil
+
+		ctx.resetUserValues()
 
 		// Remove temporary files, which may be uploaded during the request.
 		ctx.Request.RemoveMultipartFormFiles()
