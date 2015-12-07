60929d176f466413858014688356835b0a7b38e8
Aliaksandr Valialkin <valyala@gmail.com>
2015-12-07 09:11:54 +0200

    Code prettifying in fsFileReader.Read

--- fshandler.go
+++ fshandler.go
@@ -140,16 +140,18 @@ func (r *fsFileReader) Close() error {
 }
 
 func (r *fsFileReader) Read(p []byte) (int, error) {
-	if r.ff.f != nil {
-		n, err := r.ff.f.ReadAt(p, r.offset)
+	ff := r.ff
+
+	if ff.f != nil {
+		n, err := ff.f.ReadAt(p, r.offset)
 		r.offset += int64(n)
 		return n, err
 	}
 
-	if r.offset == int64(len(r.ff.dirIndex)) {
+	if r.offset == int64(len(ff.dirIndex)) {
 		return 0, io.EOF
 	}
-	n := copy(p, r.ff.dirIndex[r.offset:])
+	n := copy(p, ff.dirIndex[r.offset:])
 	r.offset += int64(n)
 	return n, nil
 }
