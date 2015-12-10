6db1946298c5ca2fd72ba97e344aad5b0ffc993f
Aliaksandr Valialkin <valyala@gmail.com>
2015-12-10 13:17:21 +0200

    Use ctx.UserAgent() shortcut instead of ctx.Request.Header.UserAgent()

--- examples/helloworldserver/helloworldserver.go
+++ examples/helloworldserver/helloworldserver.go
@@ -26,7 +26,7 @@ func requestHandler(ctx *fasthttp.RequestCtx) {
 	fmt.Fprintf(ctx, "Requested path is %q\n", ctx.Path())
 	fmt.Fprintf(ctx, "Host is %q\n", ctx.Host())
 	fmt.Fprintf(ctx, "Query string is %q\n", ctx.QueryArgs())
-	fmt.Fprintf(ctx, "User-Agent is %q\n", ctx.Request.Header.UserAgent())
+	fmt.Fprintf(ctx, "User-Agent is %q\n", ctx.UserAgent())
 	fmt.Fprintf(ctx, "Connection has been established at %s\n", ctx.ConnTime())
 	fmt.Fprintf(ctx, "Request has been started at %s\n", ctx.Time())
 	fmt.Fprintf(ctx, "Serial request number for the current connection is %d\n", ctx.ConnRequestNum())
