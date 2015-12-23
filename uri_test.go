9d4cd451d9aa4915e1901841cafcb479f451b6cf
Aliaksandr Valialkin <valyala@gmail.com>
2015-12-23 11:29:17 +0200

    test URI.Update() with path containing non-ASCII chars

--- uri_test.go
+++ uri_test.go
@@ -34,6 +34,7 @@ func TestURIUpdate(t *testing.T) {
 	testURIUpdate(t, "http://foo.bar/baz/xxx.html?aaa=22#aaa", "bb.html?xx=12#pp", "http://foo.bar/baz/bb.html?xx=12#pp")
 	testURIUpdate(t, "http://xx/a/b/c/d", "../qwe/p?zx=34", "http://xx/a/b/qwe/p?zx=34")
 	testURIUpdate(t, "https://qqq/aaa.html?foo=bar", "?baz=434&aaa", "https://qqq/aaa.html?baz=434&aaa")
+	testURIUpdate(t, "http://foo.bar/baz", "~a/%20b=c,тест?йцу=ке", "http://foo.bar/~a/%20b=c,%D1%82%D0%B5%D1%81%D1%82?йцу=ке")
 }
 
 func testURIUpdate(t *testing.T, base, update, result string) {
