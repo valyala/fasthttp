667a8905cc6a95f223b5bd41056b1ea2861118df
David Tyree <dtyree77@gmail.com>
2015-12-24 13:40:52 -0700

    Fixed bug where a post request without content-length or transfer-encoding was causing a read which blocked

--- http.go
+++ http.go
@@ -416,8 +416,18 @@ var errGetOnly = errors.New("non-GET request received")
 // RemoveMultipartFormFiles or Reset must be called after
 // reading multipart/form-data request in order to delete temporarily
 // uploaded files.
-func (req *Request) ReadLimitBody(r *bufio.Reader, maxBodySize int,w io.Writer, on100Continue func(req *Request) bool) error {
+func (req *Request) ReadLimitBody(r *bufio.Reader, maxBodySize int, w io.Writer, on100Continue func(req *Request) bool) error {
 	return req.readLimitBody(r, maxBodySize, false, w, on100Continue)
+}
+
+func (req *Request) HasBody() bool {
+	if len(req.Header.Peek("Content-Length")) > 0 && req.Header.ContentLength() > 0 {
+		return true
+	}
+	if len(req.Header.Peek("Transfer-Encoding")) != 0 {
+		return true
+	}
+	return false
 }
 
 func (req *Request) readLimitBody(r *bufio.Reader, maxBodySize int, getOnly bool, w io.Writer, on100Continue func(req *Request) bool) error {
@@ -434,11 +444,11 @@ func (req *Request) readLimitBody(r *bufio.Reader, maxBodySize int, getOnly bool
 		contentLength := req.Header.ContentLength()
 		if contentLength > 0 {
 			// See http://www.w3.org/Protocols/rfc2616/rfc2616-sec8.html for 100-Continue behavior
-			expect:=req.Header.Peek("Expect")
-			if(len(expect)>0){
+			expect := req.Header.Peek("Expect")
+			if len(expect) > 0 {
 				lowercaseBytes(expect)
-				if(bytes.Compare(expect,[]byte("100-continue"))==0){
-					if(on100Continue==nil || on100Continue(req)){
+				if bytes.Compare(expect, []byte("100-continue")) == 0 {
+					if on100Continue == nil || on100Continue(req) {
 						w.Write([]byte("HTTP/1.1 100 Continue\r\n\r\n"))
 					} else {
 						w.Write([]byte("HTTP/1.1 417 Expectation Failed\r\n\r\n"))
@@ -448,16 +458,15 @@ func (req *Request) readLimitBody(r *bufio.Reader, maxBodySize int, getOnly bool
 				}
 			}
 
-
 			// Pre-read multipart form data of known length.
 			// This way we limit memory usage for large file uploads, since their contents
 			// is streamed into temporary files if file size exceeds defaultMaxInMemoryFileSize.
 			boundary := req.Header.MultipartFormBoundary()
 			if len(boundary) > 0 {
-				if(contentLength>0){
+				if contentLength > 0 {
 					//We were stalling on waiting for an ending CRLF, RFC2616/4.1 "an HTTP/1.1 client must not preface or follow request with an extra CRLF"
 					//which was causing curl to hang waiting on a post request
-					maxBodySize=contentLength
+					maxBodySize = contentLength
 				}
 
 				req.multipartForm, err = readMultipartFormBody(r, boundary, maxBodySize, defaultMaxInMemoryFileSize)
@@ -468,12 +477,14 @@ func (req *Request) readLimitBody(r *bufio.Reader, maxBodySize int, getOnly bool
 			}
 		}
 
-		req.body, err = readBody(r, contentLength, maxBodySize, req.body)
-		if err != nil {
-			req.Reset()
-			return err
+		if req.HasBody() {
+			req.body, err = readBody(r, contentLength, maxBodySize, req.body)
+			if err != nil {
+				req.Reset()
+				return err
+			}
+			req.Header.SetContentLength(len(req.body))
 		}
-		req.Header.SetContentLength(len(req.body))
 	}
 	return nil
 }
