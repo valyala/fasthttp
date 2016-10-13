432960e4790434b344720ab55f8c5ef5e7822fe3
Aliaksandr Valialkin <valyala@gmail.com>
2016-10-13 16:17:09 +0300

    Issue #188: Add tests for empty header values' parsing

--- header_test.go
+++ header_test.go
@@ -10,6 +10,106 @@ import (
 	"testing"
 )
 
+func TestResponseHeaderEmptyValueFromHeader(t *testing.T) {
+	var h1 ResponseHeader
+	h1.SetContentType("foo/bar")
+	h1.Set("EmptyValue1", "")
+	h1.Set("EmptyValue2", " ")
+	s := h1.String()
+
+	var h ResponseHeader
+	br := bufio.NewReader(bytes.NewBufferString(s))
+	if err := h.Read(br); err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+	if string(h.ContentType()) != string(h1.ContentType()) {
+		t.Fatalf("unexpected content-type: %q. Expecting %q", h.ContentType(), h1.ContentType())
+	}
+	v1 := h.Peek("EmptyValue1")
+	if len(v1) > 0 {
+		t.Fatalf("expecting empty value. Got %q", v1)
+	}
+	v2 := h.Peek("EmptyValue2")
+	if len(v2) > 0 {
+		t.Fatalf("expecting empty value. Got %q", v2)
+	}
+}
+
+func TestResponseHeaderEmptyValueFromString(t *testing.T) {
+	s := "HTTP/1.1 200 OK\r\n" +
+		"EmptyValue1:\r\n" +
+		"Content-Type: foo/bar\r\n" +
+		"EmptyValue2: \r\n" +
+		"\r\n"
+
+	var h ResponseHeader
+	br := bufio.NewReader(bytes.NewBufferString(s))
+	if err := h.Read(br); err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+	if string(h.ContentType()) != "foo/bar" {
+		t.Fatalf("unexpected content-type: %q. Expecting %q", h.ContentType(), "foo/bar")
+	}
+	v1 := h.Peek("EmptyValue1")
+	if len(v1) > 0 {
+		t.Fatalf("expecting empty value. Got %q", v1)
+	}
+	v2 := h.Peek("EmptyValue2")
+	if len(v2) > 0 {
+		t.Fatalf("expecting empty value. Got %q", v2)
+	}
+}
+
+func TestRequestHeaderEmptyValueFromHeader(t *testing.T) {
+	var h1 RequestHeader
+	h1.SetRequestURI("/foo/bar")
+	h1.SetHost("foobar")
+	h1.Set("EmptyValue1", "")
+	h1.Set("EmptyValue2", " ")
+	s := h1.String()
+
+	var h RequestHeader
+	br := bufio.NewReader(bytes.NewBufferString(s))
+	if err := h.Read(br); err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+	if string(h.Host()) != string(h1.Host()) {
+		t.Fatalf("unexpected host: %q. Expecting %q", h.Host(), h1.Host())
+	}
+	v1 := h.Peek("EmptyValue1")
+	if len(v1) > 0 {
+		t.Fatalf("expecting empty value. Got %q", v1)
+	}
+	v2 := h.Peek("EmptyValue2")
+	if len(v2) > 0 {
+		t.Fatalf("expecting empty value. Got %q", v2)
+	}
+}
+
+func TestRequestHeaderEmptyValueFromString(t *testing.T) {
+	s := "GET / HTTP/1.1\r\n" +
+		"EmptyValue1:\r\n" +
+		"Host: foobar\r\n" +
+		"EmptyValue2: \r\n" +
+		"\r\n"
+	var h RequestHeader
+	br := bufio.NewReader(bytes.NewBufferString(s))
+	if err := h.Read(br); err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+	if string(h.Host()) != "foobar" {
+		t.Fatalf("unexpected host: %q. Expecting %q", h.Host(), "foobar")
+	}
+	v1 := h.Peek("EmptyValue1")
+	if len(v1) > 0 {
+		t.Fatalf("expecting empty value. Got %q", v1)
+	}
+	v2 := h.Peek("EmptyValue2")
+	if len(v2) > 0 {
+		t.Fatalf("expecting empty value. Got %q", v2)
+	}
+}
+
 func TestRequestHeaderSetCookieWithSpecialChars(t *testing.T) {
 	var h RequestHeader
 	h.Set("Cookie", "ID&14")
