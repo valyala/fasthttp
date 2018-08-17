3f904e14929a27c4facd9c2eb1207248955e5839
Erik Dubbelboer <erik@dubbelboer.com>
2018-08-17 16:03:16 +0800

    Remove code that does nothing
    connTime never changes so the statement does nothing.

--- server.go
+++ server.go
@@ -1576,7 +1576,6 @@ func (s *Server) serveConn(c net.Conn) error {
 		ctx.Response.Header.SetServerBytes(serverName)
 		ctx.connID = connID
 		ctx.connRequestNum = connRequestNum
-		ctx.connTime = connTime
 		ctx.time = currentTime
 		s.Handler(ctx)
 
