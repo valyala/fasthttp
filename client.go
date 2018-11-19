bc602b567ba7cb99322e58f4f721d8f0990f62b0
amezghal abdelilah <amez.goog@gmail.com>
2018-11-19 11:00:19 +0000

    cleanup

--- client.go
+++ client.go
@@ -967,6 +967,7 @@ func clientDoDeadline(req *Request, resp *Response, deadline time.Time, c client
 		if atomic.LoadInt32(&cleanup) == 1 {
 			ReleaseResponse(respCopy)
 			ReleaseRequest(reqCopy)
+			errorChPool.Put(chv)
 		} else {
 			ch <- errDo
 		}
@@ -986,7 +987,6 @@ func clientDoDeadline(req *Request, resp *Response, deadline time.Time, c client
 		errorChPool.Put(chv)
 	case <-tc.C:
 		atomic.StoreInt32(&cleanup, 1)
-		errorChPool.Put(chv)
 		err = ErrTimeout
 	}
 	releaseTimer(tc)
