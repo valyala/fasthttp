2f6ffb69ec5a47ec6366c7b76aa7440095aba9bc
andrewheberle <andrewheberle@users.noreply.github.com>
2019-08-19 10:34:40 +0800

    Create Windows stub for reuseport.Listen

---
+++ reuseport/reuseport_windows.go
@@ -0,0 +1,27 @@
+// Package reuseport provides TCP net.Listener with SO_REUSEPORT support.
+//
+// SO_REUSEPORT allows linear scaling server performance on multi-CPU servers.
+// See https://www.nginx.com/blog/socket-sharding-nginx-release-1-9-1/ for more details :)
+//
+// The package is based on https://github.com/kavu/go_reuseport .
+package reuseport
+
+import (
+	"fmt"
+  "net"
+)
+
+// ErrNoReusePort is returned if the OS doesn't support SO_REUSEPORT.
+type ErrNoReusePort struct {
+	err error
+}
+
+// Error implements error interface.
+func (e *ErrNoReusePort) Error() string {
+	return fmt.Sprint("The OS doesn't support SO_REUSEPORT: %s", e.err)
+}
+
+// Listen always returns ErrNoReusePort on Windows
+func Listen(network, addr string) (net.Listener, error) {
+	return nil, &ErrNoReusePort{fmt.Errorf("Not supported on Windows")}
+}
