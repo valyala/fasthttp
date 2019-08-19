27beb928642c5463e3f72ca49b6c8f314edf2d32
andrewheberle <andrewheberle@users.noreply.github.com>
2019-08-19 21:14:41 +0800

    Remove comment

--- reuseport/reuseport_windows.go
+++ reuseport/reuseport_windows.go
@@ -1,9 +1,3 @@
-// Package reuseport provides TCP net.Listener with SO_REUSEPORT support.
-//
-// SO_REUSEPORT allows linear scaling server performance on multi-CPU servers.
-// See https://www.nginx.com/blog/socket-sharding-nginx-release-1-9-1/ for more details :)
-//
-// The package is based on https://github.com/kavu/go_reuseport .
 package reuseport
 
 import (
