2fa960e89ac88e69178ee1354d093247d9bbf002
andrewheberle <andrewheberle@users.noreply.github.com>
2019-08-22 07:30:27 +0800

    Update reuseport_windows.go

--- reuseport/reuseport_windows.go
+++ reuseport/reuseport_windows.go
@@ -5,16 +5,6 @@ import (
 	"net"
 )
 
-// ErrNoReusePort is returned if the OS doesn't support SO_REUSEPORT.
-type ErrNoReusePort struct {
-	err error
-}
-
-// Error implements error interface.
-func (e *ErrNoReusePort) Error() string {
-	return fmt.Sprintf("The OS doesn't support SO_REUSEPORT: %s", e.err)
-}
-
 // Listen always returns ErrNoReusePort on Windows
 func Listen(network, addr string) (net.Listener, error) {
 	return nil, &ErrNoReusePort{fmt.Errorf("Not supported on Windows")}
