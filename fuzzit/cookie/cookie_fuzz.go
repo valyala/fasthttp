1241ed7a910a835656961bf69fb8f1fa9cc8b88e
Erik Dubbelboer <erik@dubbelboer.com>
2019-08-24 08:56:51 +0200

    Add fuzzit support (#634)
    * Add fuzzit support
    
    * Do local regression fuzz testing

---
+++ fuzzit/cookie/cookie_fuzz.go
@@ -0,0 +1,25 @@
+// +build gofuzz
+
+package fuzzit
+
+import (
+	"bytes"
+
+	"github.com/valyala/fasthttp"
+)
+
+func Fuzz(data []byte) int {
+	c := fasthttp.AcquireCookie()
+	defer fasthttp.ReleaseCookie(c)
+
+	if err := c.ParseBytes(data); err != nil {
+		return 0
+	}
+
+	w := bytes.Buffer{}
+	if _, err := c.WriteTo(&w); err != nil {
+		return 0
+	}
+
+	return 1
+}
---
+++ fuzzit/url/url_fuzz.go
@@ -0,0 +1,23 @@
+// +build gofuzz
+
+package fuzzit
+
+import (
+	"bytes"
+
+	"github.com/valyala/fasthttp"
+)
+
+func Fuzz(data []byte) int {
+	u := fasthttp.AcquireURI()
+	defer fasthttp.ReleaseURI(u)
+
+	u.UpdateBytes(data)
+
+	w := bytes.Buffer{}
+	if _, err := u.WriteTo(&w); err != nil {
+		return 0
+	}
+
+	return 1
+}
