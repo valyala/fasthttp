c6037ab3b463c0750efe6d853f607e1373d4f113
Erik Dubbelboer <erik@dubbelboer.com>
2019-09-16 15:03:59 +0200

    Update fuzzit

--- fuzzit.sh
+++ fuzzit.sh
@@ -11,7 +11,7 @@ go get -t -v ./...
 
 go get github.com/dvyukov/go-fuzz/go-fuzz github.com/dvyukov/go-fuzz/go-fuzz-build
 
-wget -q -O fuzzitbin https://github.com/fuzzitdev/fuzzit/releases/download/v2.4.24/fuzzit_Linux_x86_64
+wget -q -O fuzzitbin https://github.com/fuzzitdev/fuzzit/releases/download/v2.4.52/fuzzit_Linux_x86_64
 chmod a+x fuzzitbin
 
 for w in request response cookie url; do
