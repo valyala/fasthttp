571315f30a6ba21a36d70cf5c2b4e3dcb871eb04
Erik Dubbelboer <erik@dubbelboer.com>
2020-04-23 15:05:56 +0200

    Only base64 the proxy auth once

--- fasthttpproxy/http.go
+++ fasthttpproxy/http.go
@@ -18,16 +18,14 @@ import (
 //		Dial: fasthttpproxy.FasthttpHTTPDialer("username:password@localhost:9050"),
 //	}
 func FasthttpHTTPDialer(proxy string) fasthttp.DialFunc {
+	var auth string
+	if strings.Contains(proxy, "@") {
+		split := strings.Split(proxy, "@")
+		auth = base64.StdEncoding.EncodeToString([]byte(split[0]))
+		proxy = split[1]
+	}
+
 	return func(addr string) (net.Conn, error) {
-		var auth string
-
-		if strings.Contains(proxy, "@") {
-			split := strings.Split(proxy, "@")
-			auth = base64.StdEncoding.EncodeToString([]byte(split[0]))
-			proxy = split[1]
-
-		}
-
 		conn, err := fasthttp.Dial(proxy)
 		if err != nil {
 			return nil, err
