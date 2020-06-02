8c9b42365b5d572cdc9411a2e00ba4eb3d2f1db8
Andy Pan <panjf2000@gmail.com>
2020-06-02 19:45:09 +0800

    Support Windows SO_REUSEADDR

--- reuseport/reuseport_windows.go
+++ reuseport/reuseport_windows.go
@@ -1,11 +1,22 @@
 package reuseport
 
 import (
-	"fmt"
+	"context"
 	"net"
+	"syscall"
+
+	"golang.org/x/sys/windows"
 )
+
+var listenConfig = net.ListenConfig{
+	Control: func(network, address string, c syscall.RawConn) (err error) {
+		return c.Control(func(fd uintptr) {
+			err = windows.SetsockoptInt(windows.Handle(fd), windows.SOL_SOCKET, windows.SO_REUSEADDR, 1)
+		})
+	},
+}
 
 // Listen always returns ErrNoReusePort on Windows
 func Listen(network, addr string) (net.Listener, error) {
-	return nil, &ErrNoReusePort{fmt.Errorf("Not supported on Windows")}
+	return listenConfig.Listen(context.Background(), network, addr)
 }
