a151ef9795c6fb838b56c530c094fe048beec6b2
Kiyon <kiyonlin@163.com>
2020-11-19 14:31:33 +0800

    Improve round2 performance

--- http.go
+++ http.go
@@ -1956,11 +1956,13 @@ func round2(n int) int {
 	if n <= 0 {
 		return 0
 	}
-	n--
-	x := uint(0)
-	for n > 0 {
-		n >>= 1
-		x++
-	}
-	return 1 << x
+
+	x := uint32(n - 1)
+	x |= x >> 1
+	x |= x >> 2
+	x |= x >> 4
+	x |= x >> 8
+	x |= x >> 16
+
+	return int(x + 1)
 }
