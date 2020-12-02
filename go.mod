f7dad93690813eebd41075109232ea79544ab10a
calbot <calschut@gmail.com>
2020-12-02 12:30:20 -0800

    initial gnet non-blocking io support

--- go.mod
+++ go.mod
@@ -5,9 +5,10 @@ go 1.11
 require (
 	github.com/andybalholm/brotli v1.0.0
 	github.com/klauspost/compress v1.10.7
+	github.com/panjf2000/gnet v1.3.2
 	github.com/valyala/bytebufferpool v1.0.0
 	github.com/valyala/tcplisten v0.0.0-20161114210144-ceec8f93295a
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
 	golang.org/x/net v0.0.0-20201016165138-7b1cca2348c0
-	golang.org/x/sys v0.0.0-20200930185726-fdedc70b468f
+	golang.org/x/sys v0.0.0-20201116194326-cc9327a14d48
 )
--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package fasthttp
 
 import (
 	"bufio"
+	"bytes"
 	"context"
 	"crypto/tls"
 	"errors"
@@ -15,6 +16,8 @@ import (
 	"sync"
 	"sync/atomic"
 	"time"
+
+	"github.com/panjf2000/gnet"
 )
 
 var errNoCertOrKeyProvided = errors.New("cert or key has not provided")
@@ -1486,6 +1489,156 @@ func (s *Server) ListenAndServe(addr string) error {
 		}
 	}
 	return s.Serve(ln)
+}
+
+//GnetConn - Implements the net.Conn interface to allow adapting Gnet to the serveConn method
+type GnetConn struct {
+	gnetConn    gnet.Conn
+	localAddr   net.Addr
+	remoteAddr  net.Addr
+	readBuffer  *bytes.Buffer
+	writeBuffer *bytes.Buffer
+	closed      bool
+}
+
+// Read reads data from the connection.
+// Read can be made to time out and return an error after a fixed
+// time limit; see SetDeadline and SetReadDeadline.
+func (gc *GnetConn) Read(b []byte) (n int, err error) {
+	return gc.readBuffer.Read(b)
+}
+
+// Write writes data to the connection.
+// Write can be made to time out and return an error after a fixed
+// time limit; see SetDeadline and SetWriteDeadline.
+func (gc *GnetConn) Write(b []byte) (n int, err error) {
+	return gc.writeBuffer.Write(b)
+}
+
+// Close closes the connection.
+// Any blocked Read or Write operations will be unblocked and return errors.
+func (gc *GnetConn) Close() error {
+	gc.closed = true
+	return nil
+}
+
+// LocalAddr returns the local network address.
+func (gc *GnetConn) LocalAddr() net.Addr {
+	return gc.localAddr
+}
+
+// RemoteAddr returns the remote network address.
+func (gc *GnetConn) RemoteAddr() net.Addr {
+	return gc.remoteAddr
+}
+
+// SetDeadline sets the read and write deadlines associated
+// with the connection. It is equivalent to calling both
+// SetReadDeadline and SetWriteDeadline.
+//
+// A deadline is an absolute time after which I/O operations
+// fail instead of blocking. The deadline applies to all future
+// and pending I/O, not just the immediately following call to
+// Read or Write. After a deadline has been exceeded, the
+// connection can be refreshed by setting a deadline in the future.
+//
+// If the deadline is exceeded a call to Read or Write or to other
+// I/O methods will return an error that wraps os.ErrDeadlineExceeded.
+// This can be tested using errors.Is(err, os.ErrDeadlineExceeded).
+// The error's Timeout method will return true, but note that there
+// are other possible errors for which the Timeout method will
+// return true even if the deadline has not been exceeded.
+//
+// An idle timeout can be implemented by repeatedly extending
+// the deadline after successful Read or Write calls.
+//
+// A zero value for t means I/O operations will not time out.
+func (gc *GnetConn) SetDeadline(t time.Time) error {
+	return nil
+}
+
+// SetReadDeadline sets the deadline for future Read calls
+// and any currently-blocked Read call.
+// A zero value for t means Read will not time out.
+func (gc *GnetConn) SetReadDeadline(t time.Time) error {
+	return nil
+}
+
+// SetWriteDeadline sets the deadline for future Write calls
+// and any currently-blocked Write call.
+// Even if write times out, it may return n > 0, indicating that
+// some of the data was successfully written.
+// A zero value for t means Write will not time out.
+func (*GnetConn) SetWriteDeadline(t time.Time) error {
+	return nil
+}
+
+// GnetHTTP -
+type GnetHTTP struct {
+	*gnet.EventServer
+	fasthttpserver *Server
+}
+
+// OnOpened fires when a new connection has been opened.
+// The parameter:c has information about the connection such as it's local and remote address.
+// Parameter:out is the return value which is going to be sent back to the client.
+func (es *GnetHTTP) OnOpened(c gnet.Conn) (out []byte, action gnet.Action) {
+	log.Println("OPENED")
+	return
+}
+
+// OnClosed fires when a connection has been closed.
+// The parameter:err is the last known connection error.
+func (es *GnetHTTP) OnClosed(c gnet.Conn, err error) (action gnet.Action) {
+	log.Println("CLOSED")
+	return
+}
+
+// PreWrite fires just before any data is written to any client socket, this event function is usually used to
+// put some code of logging/counting/reporting or any prepositive operations before writing data to client.
+func (es *GnetHTTP) PreWrite() {
+	log.Println("PREWRITE")
+}
+
+// React fires when a connection sends the server data.
+// Call c.Read() or c.ReadN(n) within the parameter:c to read incoming data from client.
+// Parameter:out is the return value which is going to be sent back to the client.
+func (es *GnetHTTP) React(frame []byte, c gnet.Conn) (out []byte, action gnet.Action) {
+	log.Println("REACTING!!!!!")
+	gconn := &GnetConn{
+		gnetConn:    c,
+		readBuffer:  bytes.NewBuffer(frame),
+		writeBuffer: &bytes.Buffer{},
+		localAddr:   c.LocalAddr(),
+		remoteAddr:  c.RemoteAddr(),
+	}
+	c.SetContext(gconn)
+
+	//Bypasses the workpoll based implementation...
+	es.fasthttpserver.serveConn(gconn)
+
+	action = gnet.Close
+	c.ResetBuffer()
+
+	return gconn.writeBuffer.Bytes(), action
+}
+
+//ListenAndServeGnet uses gnet for non-blocking event based connections...
+func ListenAndServeGnet(addr string, handler RequestHandler) error {
+	s := &Server{
+		Handler: handler,
+	}
+	return s.ListenAndServeGnet(addr)
+}
+
+//ListenAndServeGnet uses gnet for non-blocking event based connections...
+func (s *Server) ListenAndServeGnet(addr string) error {
+	server := &GnetHTTP{fasthttpserver: s}
+	err := gnet.Serve(server, fmt.Sprintf("tcp://%v", addr), gnet.WithMulticore(true))
+	if err != nil {
+		log.Println("Error gnet serve", err)
+	}
+	return err
 }
 
 // ListenAndServeUNIX serves HTTP requests from the given UNIX addr.
