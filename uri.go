3c2d44570230cec580a433066567a72cb4fa8550
Mike Faraponov <11322032+moredure@users.noreply.github.com>
2021-03-19 16:44:28 +0200

    Update uri.go

--- uri.go
+++ uri.go
@@ -285,11 +285,11 @@ func (u *URI) parse(host, uri []byte, isTLS bool) error {
 		u.scheme = append(u.scheme[:0], strHTTPS...)
 	}
 
-	if n := bytes.Index(host, strAt); n >= 0 {
+	if n := bytes.IndexByte(host, '@'); n >= 0 {
 		auth := host[:n]
 		host = host[n+1:]
 
-		if n := bytes.Index(auth, strColon); n >= 0 {
+		if n := bytes.IndexByte(auth, ':'); n >= 0 {
 			u.username = append(u.username[:0], auth[:n]...)
 			u.password = append(u.password[:0], auth[n+1:]...)
 		} else {
