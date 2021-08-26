c7ce95f778f81d22212a24523dee746b20e91991
YenForYang <YenForYang@users.noreply.github.com>
2021-08-26 03:09:45 -0500

    Fix s2b (#1079)
    Ensure `len(b)` <= `cap(b)` at all times. Additionally, use `runtime.KeepAlive()` to prevent `s` from being GC-ed.

--- bytesconv.go
+++ bytesconv.go
@@ -15,6 +15,7 @@ import (
 	"sync"
 	"time"
 	"unsafe"
+	"runtime"
 )
 
 // AppendHTMLEscape appends html-escaped s to dst and returns the extended dst.
@@ -345,8 +346,9 @@ func s2b(s string) (b []byte) {
 	/* #nosec G103 */
 	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
 	bh.Data = sh.Data
-	bh.Len = sh.Len
 	bh.Cap = sh.Len
+	bh.Len = sh.Len
+	runtime.KeepAlive(&s)
 	return b
 }
 
