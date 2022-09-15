40eec0b70615364ab376dd4477e9f62bb5508864
Emre Savcı <freelancemuhendis@gmail.com>
2022-09-15 23:15:26 +0300

    byte to string unsafe conversion in fasthttpadaptor ConvertRequest method (#1375)
    * add byte to string unsafe conversion to fasthttpadaptor ConvertRequest method()
    
    * add nosec comment line
    
    * Update fasthttpadaptor/request.go
    
    Co-authored-by: Erik Dubbelboer <erik@dubbelboer.com>
    
    * move unsafe package import next to std packages
    
    * fix lint error in test
    
    Co-authored-by: Erik Dubbelboer <erik@dubbelboer.com>

---
+++ fasthttpadaptor/request_test.go
@@ -0,0 +1,28 @@
+package fasthttpadaptor
+
+import (
+	"github.com/valyala/fasthttp"
+	"net/http"
+	"testing"
+)
+
+func BenchmarkConvertRequest(b *testing.B) {
+	var httpReq http.Request
+
+	ctx := &fasthttp.RequestCtx{
+		Request: fasthttp.Request{
+			Header:        fasthttp.RequestHeader{},
+			UseHostHeader: false,
+		},
+	}
+	ctx.Request.Header.SetMethod("GET")
+	ctx.Request.Header.Set("x", "test")
+	ctx.Request.Header.Set("y", "test")
+	ctx.Request.SetRequestURI("/test")
+	ctx.Request.SetHost("test")
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_ = ConvertRequest(ctx, &httpReq, true)
+	}
+}
