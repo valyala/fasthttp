c599159ba44547882d24891558e540351706c6dc
orangesobeautiful <orangesobeautiful@gmail.com>
2023-02-08 13:22:02 +0800

    optimized b2s function

--- b2s_new.go
+++ b2s_new.go
@@ -8,9 +8,5 @@ import "unsafe"
 // b2s converts byte slice to a string without memory allocation.
 // See https://groups.google.com/forum/#!msg/Golang-Nuts/ENgbUzYvCuU/90yGx7GUAgAJ .
 func b2s(b []byte) string {
-	if len(b) == 0 {
-		return ""
-	}
-
-	return unsafe.String(&b[0], len(b))
+	return unsafe.String(unsafe.SliceData(b), len(b))
 }
