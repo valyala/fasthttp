1dd462561a36eeec7e1eb13c169a9a6378f859a3
Zero <54469706+3JoB@users.noreply.github.com>
2023-11-09 08:13:34 +0800

    update build constraints and annotations

--- fsutil_new.go
+++ fsutil_new.go
@@ -1,11 +1,10 @@
 //go:build go1.21
-// +build go1.21
 
 package fasthttp
 
 import "maps"
 
-// Copy global map for fs.go
+// fsMapCopy copies the given fsCompressedFileSuffixes map.
 func fsMapCopy(fsCompressedFileSuffixes map[string]string) map[string]string {
 	compressedFileSuffixes := make(map[string]string, len(fsCompressedFileSuffixes))
 	maps.Copy(compressedFileSuffixes, fsCompressedFileSuffixes)
--- fsutil_old.go
+++ fsutil_old.go
@@ -1,9 +1,8 @@
 //go:build !go1.21
-// +build !go1.21
 
 package fasthttp
 
-// Copy global map for fs.go
+// fsMapCopy copies the given fsCompressedFileSuffixes map.
 func fsMapCopy(fsCompressedFileSuffixes map[string]string) map[string]string {
 	compressedFileSuffixes := make(map[string]string, len(fsCompressedFileSuffixes))
 	for k, v := range fsCompressedFileSuffixes {
