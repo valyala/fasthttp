bf0e054f38a65bc929ad6162f7a42dbe0e9fb9aa
Evgeny <zenka712@mail.ru>
2023-12-25 16:08:12 +0300

    add IsError method like in resty client

--- http.go
+++ http.go
@@ -183,6 +183,11 @@ func (resp *Response) IsSuccess() bool {
 	return resp.StatusCode() > 199 && resp.StatusCode() < 300
 }
 
+// IsError method returns true if HTTP status `code >= 400` otherwise false.
+func (resp *Response) IsError() bool {
+	return resp.StatusCode() > 399
+}
+
 // ConnectionClose returns true if 'Connection: close' header is set.
 func (req *Request) ConnectionClose() bool {
 	return req.Header.ConnectionClose()
