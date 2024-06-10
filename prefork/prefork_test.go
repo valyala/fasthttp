9ffdf086e7d675fe979a777322dfdbc03daf6eec
Oleksandr Redko <Oleksandr_Redko@epam.com>
2024-06-10 20:08:51 +0300

    test: fix typo (#1790)

--- prefork/prefork_test.go
+++ prefork/prefork_test.go
@@ -48,7 +48,7 @@ func Test_New(t *testing.T) {
 	p := New(s)
 
 	if p.Network != defaultNetwork {
-		t.Errorf("Prefork.Netork == %q, want %q", p.Network, defaultNetwork)
+		t.Errorf("Prefork.Network == %q, want %q", p.Network, defaultNetwork)
 	}
 
 	if reflect.ValueOf(p.ServeFunc).Pointer() != reflect.ValueOf(s.Serve).Pointer() {
