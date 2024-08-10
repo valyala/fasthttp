dbf3caac7fd1fb332e00b2ac873f1e7e87d99439
ksw2000 <13825170+ksw2000@users.noreply.github.com>
2024-08-11 00:29:31 +0800

    Reduce sizeof ResponseHeader and RequestHeader
    + Reduce ResponseHeader from 320 to 312 bytes
    + Reduce RequestHeader from 360 to 352 bytes
    + In the benchmark tests, although there is no significant performance improvement, it theoretically reduces memory usage by 2.2% to 2.5%.

--- http.go
+++ http.go
@@ -1600,10 +1600,10 @@ func (req *Request) Write(w *bufio.Writer) error {
 			nl := len(uri.username) + len(uri.password) + 1
 			nb := nl + len(strBasicSpace)
 			tl := nb + base64.StdEncoding.EncodedLen(nl)
-			if tl > cap(req.Header.bufKV.value) {
-				req.Header.bufKV.value = make([]byte, 0, tl)
+			if tl > cap(req.Header.bufV) {
+				req.Header.bufV = make([]byte, 0, tl)
 			}
-			buf := req.Header.bufKV.value[:0]
+			buf := req.Header.bufV[:0]
 			buf = append(buf, uri.username...)
 			buf = append(buf, strColon...)
 			buf = append(buf, uri.password...)
