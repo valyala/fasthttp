76bf19755f94119cd1effa055d39a8b9916bb3de
ksw2000 <13825170+ksw2000@users.noreply.github.com>
2024-12-19 07:58:52 +0000

    chore: merge upstream PR #6 from valyala/tcplisten: Backport TCP backlog size update of uint16->uint32 with Linux

--- tcplisten/tcplisten_linux.go
+++ tcplisten/tcplisten_linux.go
@@ -1,10 +1,9 @@
-// +build linux
+//go:build linux
 
 package tcplisten
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"strconv"
 	"strings"
@@ -33,7 +32,7 @@ func enableFastOpen(fd int) error {
 const fastOpenQlen = 16 * 1024
 
 func soMaxConn() (int, error) {
-	data, err := ioutil.ReadFile(soMaxConnFilePath)
+	data, err := os.ReadFile(soMaxConnFilePath)
 	if err != nil {
 		// This error may trigger on travis build. Just use SOMAXCONN
 		if os.IsNotExist(err) {
@@ -51,9 +50,66 @@ func soMaxConn() (int, error) {
 	// Truncate number to avoid wrapping.
 	// See https://github.com/golang/go/issues/5030 .
 	if n > 1<<16-1 {
-		n = 1<<16 - 1
+		n = maxAckBacklog(n)
 	}
 	return n, nil
+}
+func kernelVersion() (major int, minor int) {
+	var uname syscall.Utsname
+	if err := syscall.Uname(&uname); err != nil {
+		return
+	}
+
+	rl := uname.Release
+	var values [2]int
+	vi := 0
+	value := 0
+	for _, c := range rl {
+		if c >= '0' && c <= '9' {
+			value = (value * 10) + int(c-'0')
+		} else {
+			// Note that we're assuming N.N.N here.  If we see anything else we are likely to
+			// mis-parse it.
+			values[vi] = value
+			vi++
+			if vi >= len(values) {
+				break
+			}
+		}
+	}
+	switch vi {
+	case 0:
+		return 0, 0
+	case 1:
+		return values[0], 0
+	case 2:
+		return values[0], values[1]
+	}
+	return
+}
+
+// Linux stores the backlog as:
+//
+//   - uint16 in kernel version < 4.1,
+//   - uint32 in kernel version >= 4.1
+//
+// Truncate number to avoid wrapping.
+//
+// See issue 5 or
+// https://github.com/golang/go/issues/5030.
+// https://github.com/golang/go/issues/41470.
+func maxAckBacklog(n int) int {
+	major, minor := kernelVersion()
+	size := 16
+	if major > 4 || (major == 4 && minor >= 1) {
+		size = 32
+	}
+
+	var max uint = 1<<size - 1
+	if uint(n) > max {
+		n = int(max)
+	}
+	return n
 }
 
 const soMaxConnFilePath = "/proc/sys/net/core/somaxconn"
