711e7dd17d66ab3bf878894c91864bf7d794094f
pjebs <pj@pjebs.com.au>
2025-02-13 22:29:30 +1100

    add dummy support for js,wasm

--- tcplisten/socket_other.go
+++ tcplisten/socket_other.go
@@ -1,4 +1,4 @@
-//go:build linux || dragonfly || freebsd || netbsd || openbsd || rumprun
+//go:build !js && !wasm && (linux || dragonfly || freebsd || netbsd || openbsd || rumprun)
 
 package tcplisten
 
