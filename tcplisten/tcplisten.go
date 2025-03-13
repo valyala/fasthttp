30b09beff11be4282c37ef6b7d8854589d565447
Erik Dubbelboer <erik@dubbelboer.com>
2025-03-13 04:58:07 +0100

    Fix untyped int constant 4294967295
    Fix: https://github.com/valyala/fasthttp/issues/1960

--- tcplisten/tcplisten.go
+++ tcplisten/tcplisten.go
@@ -184,8 +184,9 @@ func safeIntToUint32(i int) (uint32, error) {
 	if i < 0 {
 		return 0, errors.New("value is negative, cannot convert to uint32")
 	}
-	if i > math.MaxUint32 {
+	ui := uint64(i)
+	if ui > math.MaxUint32 {
 		return 0, errors.New("value exceeds uint32 max value")
 	}
-	return uint32(i), nil
+	return uint32(ui), nil
 }
