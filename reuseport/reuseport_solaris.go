1d8fe19359b6f308806a2773713bfde7f13ddf29
jwntree <84563040+jwntree@users.noreply.github.com>
2025-08-04 23:27:16 +0900

    Add reuseport support for Solaris (#2046)

---
+++ reuseport/reuseport_solaris.go
@@ -0,0 +1,31 @@
+//go:build solaris
+
+package reuseport
+
+import (
+	"context"
+	"net"
+	"syscall"
+
+	"golang.org/x/sys/unix"
+)
+
+const (
+	_SO_REUSEPORT = 0x100e
+)
+
+var listenConfig = net.ListenConfig{
+	Control: func(network, address string, c syscall.RawConn) (err error) {
+		return c.Control(func(fd uintptr) {
+			err = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_REUSEADDR, 1)
+			if err == nil {
+				err = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, _SO_REUSEPORT, 1)
+			}
+		})
+	},
+}
+
+// Listen returns a TCP listener with the SO_REUSEADDR and SO_REUSEPORT options set.
+func Listen(network, addr string) (net.Listener, error) {
+	return listenConfig.Listen(context.Background(), network, addr)
+}
