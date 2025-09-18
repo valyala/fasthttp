6c2101ef4034fd10eb0fd1a842ca93ebbb4d5548
Giovanni Rivera <rivera.giovanni271@gmail.com>
2025-09-17 21:48:07 -0700

    tests(fasthttphandler): Add resource recycling unit tests

--- fasthttpadaptor/adaptor_test.go
+++ fasthttpadaptor/adaptor_test.go
@@ -2,9 +2,13 @@ package fasthttpadaptor
 
 import (
 	"bufio"
+	"bytes"
+	"fmt"
 	"io"
 	"net"
 	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
 	"net/url"
 	"reflect"
 	"testing"
@@ -97,6 +101,132 @@ func TestNewFastHTTPHandler(t *testing.T) {
 		w.Write(body) //nolint:errcheck
 	}
 	fasthttpH := NewFastHTTPHandler(http.HandlerFunc(nethttpH))
+	fasthttpH = setContextValueMiddleware(fasthttpH, expectedContextKey, expectedContextValue)
+
+	var ctx fasthttp.RequestCtx
+	var req fasthttp.Request
+
+	req.Header.SetMethod(expectedMethod)
+	req.SetRequestURI(expectedRequestURI)
+	req.Header.SetHost(expectedHost)
+	req.BodyWriter().Write([]byte(expectedBody)) //nolint:errcheck
+	for k, v := range expectedHeader {
+		req.Header.Set(k, v)
+	}
+
+	remoteAddr, err := net.ResolveTCPAddr("tcp", expectedRemoteAddr)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	ctx.Init(&req, remoteAddr, nil)
+
+	fasthttpH(&ctx)
+
+	if callsCount != 1 {
+		t.Fatalf("unexpected callsCount: %d. Expecting 1", callsCount)
+	}
+
+	resp := &ctx.Response
+	if resp.StatusCode() != fasthttp.StatusBadRequest {
+		t.Fatalf("unexpected statusCode: %d. Expecting %d", resp.StatusCode(), fasthttp.StatusBadRequest)
+	}
+	if string(resp.Header.Peek("Header1")) != "value1" {
+		t.Fatalf("unexpected header value: %q. Expecting %q", resp.Header.Peek("Header1"), "value1")
+	}
+	if string(resp.Header.Peek("Header2")) != "value2" {
+		t.Fatalf("unexpected header value: %q. Expecting %q", resp.Header.Peek("Header2"), "value2")
+	}
+	if string(resp.Body()) != expectedBody {
+		t.Fatalf("unexpected response body %q. Expecting %q", resp.Body(), expectedBody)
+	}
+	if string(resp.Header.Peek("Content-Type")) != expectedContentType {
+		t.Fatalf("unexpected response content-type %q. Expecting %q", string(resp.Header.Peek("Content-Type")), expectedContentType)
+	}
+}
+
+func TestNewFastHTTPHandlerFunc(t *testing.T) {
+	t.Parallel()
+
+	expectedMethod := fasthttp.MethodPost
+	expectedProto := "HTTP/1.1"
+	expectedProtoMajor := 1
+	expectedProtoMinor := 1
+	expectedRequestURI := "/foo/bar?baz=123"
+	expectedBody := "<!doctype html><html>"
+	expectedContentLength := len(expectedBody)
+	expectedHost := "foobar.com"
+	expectedRemoteAddr := "1.2.3.4:6789"
+	expectedHeader := map[string]string{
+		"Foo-Bar":         "baz",
+		"Abc":             "defg",
+		"XXX-Remote-Addr": "123.43.4543.345",
+	}
+	expectedURL, err := url.ParseRequestURI(expectedRequestURI)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	expectedContextKey := "contextKey"
+	expectedContextValue := "contextValue"
+	expectedContentType := "text/html; charset=utf-8"
+
+	callsCount := 0
+	nethttpH := func(w http.ResponseWriter, r *http.Request) {
+		callsCount++
+		if r.Method != expectedMethod {
+			t.Fatalf("unexpected method %q. Expecting %q", r.Method, expectedMethod)
+		}
+		if r.Proto != expectedProto {
+			t.Fatalf("unexpected proto %q. Expecting %q", r.Proto, expectedProto)
+		}
+		if r.ProtoMajor != expectedProtoMajor {
+			t.Fatalf("unexpected protoMajor %d. Expecting %d", r.ProtoMajor, expectedProtoMajor)
+		}
+		if r.ProtoMinor != expectedProtoMinor {
+			t.Fatalf("unexpected protoMinor %d. Expecting %d", r.ProtoMinor, expectedProtoMinor)
+		}
+		if r.RequestURI != expectedRequestURI {
+			t.Fatalf("unexpected requestURI %q. Expecting %q", r.RequestURI, expectedRequestURI)
+		}
+		if r.ContentLength != int64(expectedContentLength) {
+			t.Fatalf("unexpected contentLength %d. Expecting %d", r.ContentLength, expectedContentLength)
+		}
+		if len(r.TransferEncoding) != 0 {
+			t.Fatalf("unexpected transferEncoding %q. Expecting []", r.TransferEncoding)
+		}
+		if r.Host != expectedHost {
+			t.Fatalf("unexpected host %q. Expecting %q", r.Host, expectedHost)
+		}
+		if r.RemoteAddr != expectedRemoteAddr {
+			t.Fatalf("unexpected remoteAddr %q. Expecting %q", r.RemoteAddr, expectedRemoteAddr)
+		}
+		body, err := io.ReadAll(r.Body)
+		r.Body.Close()
+		if err != nil {
+			t.Fatalf("unexpected error when reading request body: %v", err)
+		}
+		if string(body) != expectedBody {
+			t.Fatalf("unexpected body %q. Expecting %q", body, expectedBody)
+		}
+		if !reflect.DeepEqual(r.URL, expectedURL) {
+			t.Fatalf("unexpected URL: %#v. Expecting %#v", r.URL, expectedURL)
+		}
+		if r.Context().Value(expectedContextKey) != expectedContextValue {
+			t.Fatalf("unexpected context value for key %q. Expecting %q", expectedContextKey, expectedContextValue)
+		}
+
+		for k, expectedV := range expectedHeader {
+			v := r.Header.Get(k)
+			if v != expectedV {
+				t.Fatalf("unexpected header value %q for key %q. Expecting %q", v, k, expectedV)
+			}
+		}
+
+		w.Header().Set("Header1", "value1")
+		w.Header().Set("Header2", "value2")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(body) //nolint:errcheck
+	}
+	fasthttpH := NewFastHTTPHandlerFunc(nethttpH)
 	fasthttpH = setContextValueMiddleware(fasthttpH, expectedContextKey, expectedContextValue)
 
 	var ctx fasthttp.RequestCtx
@@ -429,4 +559,175 @@ func TestHijackFlush(t *testing.T) {
 	case <-time.After(time.Second):
 		t.Fatal("timeout")
 	}
+}
+
+func TestResourceRecyclingUnderLoad_OneEndpoint(t *testing.T) {
+	t.Parallel()
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World!")
+	}
+
+	s := &fasthttp.Server{
+		Handler: NewFastHTTPHandler(http.HandlerFunc(handler)),
+	}
+
+	requestCount := 10
+	responseTimeout := 500 * time.Millisecond
+	expectedBody := "Hello World!"
+
+	ln := fasthttputil.NewInmemoryListener()
+	defer ln.Close()
+
+	go func() {
+		if err := s.Serve(ln); err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+	}()
+
+	for reqID := 1; reqID <= requestCount; reqID++ {
+		req := httptest.NewRequest("GET", "/", nil)
+		resp, err := sendRequest(ln, req, responseTimeout)
+		if err != nil {
+			t.Errorf("[%d] unexpected error sending request: %v", reqID, err)
+		}
+		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Errorf("[%d] unexpected error reading body: %v", reqID, err)
+		}
+
+		if string(body) != expectedBody {
+			t.Errorf("[%d] unexpected response: %q. Expecting %q", reqID, body, expectedBody)
+		}
+	}
+}
+
+func TestResourceRecyclingUnderLoad_MultipleEndpoints(t *testing.T) {
+	t.Parallel()
+
+	handlers := []struct {
+		endpoint     string
+		handler      fasthttp.RequestHandler
+		expectedBody string
+	}{
+		{
+			endpoint: "/done",
+			handler: NewFastHTTPHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				fmt.Fprintf(w, "Hello World!")
+			})),
+			expectedBody: "Hello World!",
+		},
+		{
+			endpoint: "/flush",
+			handler: NewFastHTTPHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				if f, ok := w.(http.Flusher); ok {
+					w.Write([]byte("foo")) //nolint:errcheck
+					f.Flush()
+					time.Sleep(250 * time.Millisecond)
+					w.Write([]byte("bar")) //nolint:errcheck
+					f.Flush()
+				} else {
+					http.Error(w, "Flusher not supported", http.StatusInternalServerError)
+				}
+			})),
+			expectedBody: "foobar",
+		},
+		{
+			endpoint: "/hijack",
+			handler: NewFastHTTPHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				if hj, ok := w.(http.Hijacker); ok {
+					conn, rw, err := hj.Hijack()
+					if err != nil {
+						t.Errorf("unexpected error: %v", err)
+						return
+					}
+					defer conn.Close()
+					rw.WriteString("hijacked") //nolint:errcheck
+					rw.Flush()                 //nolint:errcheck
+				} else {
+					http.Error(w, "Hijacker not supported", http.StatusInternalServerError)
+				}
+			})),
+			expectedBody: "hijacked",
+		},
+	}
+
+	s := &fasthttp.Server{
+		Handler: func(ctx *fasthttp.RequestCtx) {
+			for _, h := range handlers {
+				if string(ctx.Path()) == h.endpoint {
+					h.handler(ctx)
+					return
+				}
+			}
+			ctx.Error("Not Found", fasthttp.StatusNotFound)
+		},
+	}
+
+	repeatCount := 3
+	responseTimeout := 500 * time.Millisecond
+
+	ln := fasthttputil.NewInmemoryListener()
+	defer ln.Close()
+
+	go func() {
+		if err := s.Serve(ln); err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+	}()
+
+	for range repeatCount {
+		for _, handler := range handlers {
+			req := httptest.NewRequest("GET", handler.endpoint, nil)
+			resp, err := sendRequest(ln, req, responseTimeout)
+			if err != nil {
+				t.Errorf("[%s] unexpected error sending request: %v", handler.endpoint, err)
+			}
+			defer resp.Body.Close()
+
+			body, err := io.ReadAll(resp.Body)
+			if err != nil && err != io.ErrUnexpectedEOF {
+				t.Errorf("[%s] unexpected error reading body: %v", handler.endpoint, err)
+			}
+			if string(body) != handler.expectedBody {
+				t.Errorf("[%s] unexpected response: %q. Expecting %q", handler.endpoint, body, handler.expectedBody)
+			}
+		}
+	}
+}
+
+func sendRequest(ln *fasthttputil.InmemoryListener, req *http.Request, responseTimeout time.Duration) (*http.Response, error) {
+	c, err := ln.Dial()
+	if err != nil {
+		return nil, err
+	}
+
+	dump, err := httputil.DumpRequest(req, true)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := c.Write(dump); err != nil {
+		return nil, err
+	}
+
+	time.AfterFunc(responseTimeout, func() {
+		c.Close()
+	})
+	body, err := io.ReadAll(c)
+	if err != nil {
+		return nil, err
+	}
+
+	// Attempt to read as a response.
+	resp, err := http.ReadResponse(bufio.NewReader(bytes.NewReader(body)), nil)
+	if err != nil {
+		// Might be a hijacked connection. Read as a bare-bones body.
+		resp = &http.Response{
+			Body: io.NopCloser(bytes.NewReader(body)),
+		}
+	}
+	return resp, nil
 }
