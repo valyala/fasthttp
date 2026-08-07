ef89365cc0865c1dd6c4955e50ef9883e9c17c14
耗子 <haozi@loli.email>
2026-08-08 02:39:24 +0800

    name the HijackRejecter optional interface
    The anonymous interface left third-party protocol streams no discoverable
    way to observe a rejected RequestCtx.Hijack, which returns no error. Name
    and document it beside the other optional ProtocolStream interfaces, and
    assert the HTTP/2 stream implements it.

--- server.go
+++ server.go
@@ -822,7 +822,7 @@ type HijackHandler func(c net.Conn)
 //   - HTTP/2.0 ( https://en.wikipedia.org/wiki/HTTP/2 )
 func (ctx *RequestCtx) Hijack(handler HijackHandler) {
 	if ctx.TryHijack(handler) != nil {
-		if rejecter, ok := ctx.protocolStream.(interface{ RejectHijack() }); ok {
+		if rejecter, ok := ctx.protocolStream.(HijackRejecter); ok {
 			rejecter.RejectHijack()
 		}
 	}
