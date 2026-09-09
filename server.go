f9fa323f0c969ea06b0fe97766afd1c2ea679c0f
Vedant Madane <6527493+VedantMadane@users.noreply.github.com>
2026-09-09 18:35:31 +0530

    fix(request): warn when Request.Body() is called on streamed requests
    Signed-off-by: Vedant Madane <6527493+VedantMadane@users.noreply.github.com>

--- server.go
+++ server.go
@@ -2442,6 +2442,7 @@ func (s *Server) serveConnCounted(c net.Conn, countConcurrency bool) error {
 		}
 
 		ctx.Request.isTLS = isTLS
+		ctx.Request.logger = s.logger()
 		ctx.Response.Header.noDefaultContentType = s.NoDefaultContentType
 		ctx.Response.Header.noDefaultDate = s.NoDefaultDate
 
@@ -2968,6 +2969,7 @@ func (ctx *RequestCtx) Init2(conn net.Conn, logger Logger, reduceMemoryUsage boo
 	ctx.c = conn
 	ctx.remoteAddr = nil
 	ctx.logger.logger = logger
+	ctx.Request.logger = logger
 	ctx.connID = nextConnID()
 	ctx.s = fakeServer
 	ctx.connRequestNum = 0
@@ -2996,6 +2998,7 @@ func (ctx *RequestCtx) Init(req *Request, remoteAddr net.Addr, logger Logger) {
 	}
 	ctx.Init2(c, logger, true)
 	req.CopyTo(&ctx.Request)
+	ctx.Request.logger = logger
 }
 
 // Deadline returns the time when work done on behalf of this context
